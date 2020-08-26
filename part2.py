0e29eed7e5f8c78ba82c8a49dc865d4a851dd914
Deepak Kumar Verma <dkvermajbp@gmail.com>
2020-08-27 05:19:16 +0530

    Add files via upload

--- part2.py
+++ part2.py
@@ -1,33 +1,36 @@
-#cleaning tweets and creating word frequency for each 4 tables
-import string
-import json
-import csv
-import tweepy
-import re
+'''
+cleaning all tables
+'''
 import pandas as pd
-import numpy as np
+import re
 
-#import my csv file
-df = pd.read_csv("unsmile.csv")
-#{happy,fun,sad,unsmile}
+df = pd.read_csv('happy.csv')
+#{sad,unsmile,happy,fun}
 #Remove any rows with a "nan" in them
 df = df.dropna(axis=0, how = 'any')
 
-#Here I get each unique keyword from my dataframe
-array = df['text'].str.split(' ', expand=True).stack().value_counts()
-#print(array) to see what this looks like
-#I make a dataframe of the words and the frequency with which the words appear 
-d = {'word': array.index, 'frequency':array}
-df2 = pd.DataFrame(data = d)
+#Make it so that any non readable text gets converted into nothing
+def removetext(text):
+    return ''.join([i if ord(i) < 128 else '' for i in text])
 
-#I get rid of any words that are mentioned less than 10 times
-df2['frequency'] = df2['frequency'][df2['frequency'] > 10]
+#Here I am doing the actual removing
+df['text'] = df['text'].apply(removetext)
 
-#Remove any rows with a "nan" in them
-df2 = df2.dropna(axis=0, how = 'any')
-df2.head(10)
-#Drop any obvious signs of these words being :(
-df2 = df2.drop(index=[':(','https://t', ''], axis = 0)
-#Convert my dataframe into a csv file
-df2.to_csv('unsmile_words.csv', header=True, index=False, encoding='utf-8')
-#{happy,fun,sad,unsmile}
+#Make all my texts lower case
+df['text'] = df['text'].apply(lambda x: x.lower())
+
+#Get rid of all weird punctuation and extra lines
+df['text'] = df['text'].apply(lambda x: x.replace('.',' '))
+df['text'] = df['text'].apply(lambda x: x.replace('\n',' '))
+df['text'] = df['text'].apply(lambda x: x.replace('?',' '))
+df['text'] = df['text'].apply(lambda x: x.replace('!',' '))
+df['text'] = df['text'].apply(lambda x: x.replace('"',' '))
+df['text'] = df['text'].apply(lambda x: x.replace(';',' '))
+df['text'] = df['text'].apply(lambda x: x.replace('#',' '))
+df['text'] = df['text'].apply(lambda x: x.replace(',',' '))
+df['text'] = df['text'].apply(lambda x: x.replace('&amp',' '))
+df['text'] = df['text'].apply(lambda x: x.replace("i'm","im"))
+df['text'] = df['text'].apply(lambda x: x.replace("it's","its"))
+
+df.to_csv('fun.csv', header=True, index=False, encoding='utf-8')
+#{sad,unsmile,happy,fun}
